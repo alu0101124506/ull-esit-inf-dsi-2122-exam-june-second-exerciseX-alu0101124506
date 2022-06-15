b0b4b92bcbfe5292ac4b7d2bf8ff838ac4ee7e0e
Micaela Lucia Mungay Juncal <alu0101124506@ull.edu.es>
2022-06-15 10:37:46 +0000

    Actualizacion ejercicio 1 y su spec

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -34,8 +34,7 @@ export class Backpack {
      * Ordena el array tupla segun el ratio peso/beneficio de los objetos
      */
     sortPesoBeneficio(): [number, number][] {
-        this.pesoBeneficio = this.pesoBeneficio.sort((n1, n2) => n2[0]/n1[1] - n1[1]/n2[1]);
-        this.pesoBeneficio.reverse();
+        this.pesoBeneficio = this.pesoBeneficio.sort((n1, n2) => n2[0]/n2[1] - n1[0]/n1[1]);
         return this.pesoBeneficio;
     }
 
@@ -46,7 +45,7 @@ export class Backpack {
     fillBackpackVoraz(): number {
         let backpack = 0;
         this.pesoBeneficio.forEach(ratio => {
-            if ((backpack + ratio[0]) < this.pesomax) {
+            if ((backpack + ratio[0]) <= this.pesomax) {
                 backpack = backpack + ratio[0];
             }
         });
--- tests/ejercicio1.spec.ts
+++ tests/ejercicio1.spec.ts
@@ -7,9 +7,9 @@ describe('Comprobamos el funcionamiento de Backpack', () => {
       expect(new Backpack([[1, 5], [4, 8], [9, 1], [8, 2]], 5)).to.be.instanceOf(Backpack);
     });
     it('Comprobamos el funcionamiento de sortPesoBeneficio', () => {
-        expect(new Backpack([[1, 5], [4, 8], [9, 1], [8, 2]], 5).sortPesoBeneficio()).to.be.eql([[8, 2], [9, 1], [1, 5], [4, 8]]);
+        expect(new Backpack([[1, 5], [4, 8], [9, 1], [8, 2]], 5).sortPesoBeneficio()).to.be.eql([[9, 1], [8, 2], [4, 8], [1, 5]]);
     });
     it('Comprobamos el funcionamiento de fillBackpack', () => {
-        expect(new Backpack([[1, 5], [4, 8], [9, 1], [8, 2]], 5).fillBackpackVoraz()).to.be.eql(1);
+        expect(new Backpack([[1, 5], [4, 8], [9, 1], [8, 2]], 5).fillBackpackVoraz()).to.be.eql(5);
     });    
 });
